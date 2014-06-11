0fd0fd0e65d03fa9f5a282d36ef02bd6361081cd
Paul Irish <paul.irish@gmail.com>
2014-06-11 13:05:45 -0700

    it works.

---
+++ app/scripts/background.js
@@ -0,0 +1,61 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    console.log('previousVersion', details.previousVersion);
+});
+
+chrome.browserAction.setBadgeText({text: '\'sup'});
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+
+    var tabId = tab.id;
+
+    var protocolVersion = '1.1';
+    chrome.debugger.attach({
+        tabId: tabId
+    }, protocolVersion, function() {
+        if (chrome.runtime.lastError) {
+            console.log(chrome.runtime.lastError.message);
+            return;
+        }
+        // 2. Debugger attached, now prepare for modifying the UA
+        chrome.debugger.sendCommand({
+            tabId: tabId
+        }, "Page.enable", {}, function(response) {
+            // Possible response: response.id / response.error
+            // 3. do stuff
+
+            chrome.debugger.sendCommand({
+                tabId: tabId
+            }, "Page.setDeviceMetricsOverride",{
+                width: 300,
+                height: 300,
+                deviceScaleFactor: 2,
+                emulateViewport : true,
+                fitWindow: false
+            }, function(response) {
+                 console.log('metrics!', arguments);
+                // chrome.debugger.detach({tabId: tabId});
+            });
+
+
+            chrome.debugger.sendCommand({
+                tabId: tabId
+            }, "Page.reload",{
+                ignoreCache : true
+            }, function(response) {
+                 console.log('reloaded!', arguments);
+                // chrome.debugger.detach({tabId: tabId});
+            });
+
+
+
+
+        });
+    });
+
+
+});
+
+
+console.log('\'Allo \'Allo! Event Page for Browser Action');
---
+++ app/scripts/popup.js
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log('\'Allo \'Allo! Popup');
+
+debugger;
