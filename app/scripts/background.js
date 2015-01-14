b6ea1213ff6439f87a5868dfa3b33b3c0816cfb9
Paul Irish <paul.irish@gmail.com>
2015-01-14 14:25:19 -0800

    yah

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -130,7 +130,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: 'mobile'});
+//chrome.browserAction.setBadgeText({text: 'mobile'});
 
 chrome.browserAction.onClicked.addListener(launchPopup);
 
