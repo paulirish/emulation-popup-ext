7b13ffae7b15928f0d2a6caab4f20c84144e0347
Paul Irish <paul.irish@gmail.com>
2017-04-03 18:34:28 -0700

    diff shit.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,31 +1,21 @@
 'use strict';
 
-var tabId, popUrl;
+/* global chrome */
+/* eslint-disable no-console */
 
-// stolen from https://code.google.com/p/chromium/codesearch#chromium/src/third_party/WebKit/Source/devtools/front_end/toolbox/OverridesUI.js&q=%22Nexus%204%22&sq=package:chromium&type=cs&l=315
-var phonesArray = [
-    {title: "Apple iPhone 4", width: 320, height: 480, deviceScaleFactor: 2, userAgent: "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5", touch: true, mobile: true},
-    {title: "Apple iPhone 5", width: 320, height: 568, deviceScaleFactor: 2, userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53", touch: true, mobile: true},
+var tabId;
+var popUrl = "https://drive.google.com/corp/drive/u/0/mobile/search";
 
-    {title: "Google Nexus 4", width: 384, height: 640, deviceScaleFactor: 2, userAgent: "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19", touch: true, mobile: true},
-    {title: "Google Nexus 5", width: 360, height: 640, deviceScaleFactor: 3, userAgent: "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19", touch: true, mobile: true},
-    {title: "Google Nexus S", width: 320, height: 533, deviceScaleFactor: 1.5, userAgent: "Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; Nexus S Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1", touch: true, mobile: true}
-    ];
+var chromeRegex = new RegExp('(?:^|\\W)Chrome/(\\S+)');
+var chromeMatch = navigator.userAgent.match(chromeRegex);
+const mobileUA = `Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${chromeMatch[1]} Mobile Safari/537.36`;
 
-var phones = {};
-phonesArray.forEach(function(phone){
-    phones[phone.title.replace(/\s+/gi,'')] = phone;
-});
-// thus…
-// phones.AppleiPhone4
-// phones.AppleiPhone5
-// phones.GoogleNexus4
-// phones.GoogleNexus5
-// phones.GoogleNexusS
+console.log({ mobileUA })
 
-// usage:
-// phones.GoogleNexus4.width , etc.
+const height = 200;
+const width = 400;
 
+// https://drive.google.com/corp/drive/u/0/mobile/search
 
 // the good stuff.
 function turnItOn(tabId){
@@ -36,34 +26,22 @@ function turnItOn(tabId){
     chrome.debugger.sendCommand({
         tabId: tabId
     }, "Network.setUserAgentOverride",{
-        userAgent : phones.GoogleNexus4.userAgent
+        userAgent : mobileUA
     }, function() {
-         console.log('useragent in place!', arguments);
 
-        // set up device metrics
-        chrome.debugger.sendCommand({
-            tabId: tabId
-        }, "Page.setDeviceMetricsOverride",{
-            width:              phones.GoogleNexus4.width,
-            height:             phones.GoogleNexus4.height,
-            deviceScaleFactor:  phones.GoogleNexus4.deviceScaleFactor,
-            mobile:             phones.GoogleNexus4.mobile,
-            fitWindow: false
-        }, function() {
-             console.log('metrics!', arguments);
+        console.log('useragent in place!', arguments);
 
-            // reload page
+
+        setTimeout(_ => {
             chrome.debugger.sendCommand({
                 tabId: tabId
             }, "Page.navigate",{
                 url : popUrl
             }, function() {
-                 console.log('navigated!', arguments);
-                // chrome.debugger.detach({tabId: tabId});
+                console.log('navigated!', arguments);
+                chrome.debugger.detach({tabId: tabId});
             });
-
-        });
-
+         }, 50);
 
     });
 
@@ -72,25 +50,16 @@ function turnItOn(tabId){
 
 function launchPopup(tab){
     chrome.windows.create({
-        "width" :   phones.GoogleNexus4.width,
-        "height" :  phones.GoogleNexus4.height,
+        "height" :  height,
+        "width" :   width,
         // "top" : 100,
         // "left" : 100,
         "type" : "popup",
         "url" : 'about:blank'}
     , function(windows){
 
-        //save the url we want to load in global for use later 
-        popUrl = tab.url;
-
-        // attach listener for when the tab in the new window loads.
         var newTabId = windows.tabs[0].id;
-        chrome.tabs.onUpdated.addListener( function(updatedTabId, changeInfo, tab) {
-            if(updatedTabId===newTabId && changeInfo.status == "complete"){ 
-                // use new tabID
-                attachDebugger(newTabId);
-            }
-        } );
+        setTimeout(_ => attachDebugger(newTabId), 20);
     });
 
 }
@@ -107,22 +76,8 @@ function attachDebugger(tabId) {
             return;
         }
         // 2. Debugger attached, now prepare for modifying the UA
-        chrome.debugger.sendCommand({
-            tabId: tabId
-        }, "Network.enable", {}, function(response) {
-            // Possible response: response.id / response.error
-            // 3. do stuff
+        turnItOn(tabId);
 
-            chrome.debugger.sendCommand({
-                tabId: tabId
-            }, "Page.enable", {}, function(response) {
-
-                // oh yeah
-                turnItOn(tabId);
-            });
-
-
-        });
     });
 
 }
