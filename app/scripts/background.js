41c49bc9783cf41f442f0f01dd1bd773d2858ff5
Paul Irish <paul.irish@gmail.com>
2017-04-04 10:55:06 -0700

    boring extension apis are better sometimes.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,81 +3,43 @@
 /* global chrome */
 /* eslint-disable no-console */
 
-let tabId;
-var popUrl = 'https://drive.google.com/corp/drive/u/0/mobile/search';
+const driveSearchURL = 'https://drive.google.com/corp/drive/u/0/mobile/search';
 
-var chromeRegex = new RegExp('(?:^|\\W)Chrome/(\\S+)');
-var chromeMatch = navigator.userAgent.match(chromeRegex);
+const chromeRegex = new RegExp('(?:^|\\W)Chrome/(\\S+)');
+const chromeMatch = navigator.userAgent.match(chromeRegex);
 const userAgent = `Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${chromeMatch[1]} Mobile Safari/537.36`;
 
-console.log({userAgent});
+const height = 315;
+const width = 475;
 
-const height = 200;
-const width = 400;
+let enabled = false;
 
-const sendCommand = (command, params) => {
-  return new Promise((resolve, reject) => {
-    chrome.debugger.sendCommand({tabId}, command, params, result => {
-      if (chrome.runtime.lastError) {
-        // The error from the extension has a `message` property that is the
-        // stringified version of the actual protocol error object.
-        const message = chrome.runtime.lastError.message;
-        let errorMessage;
-        try {
-          errorMessage = JSON.parse(message).message;
-        } catch (e) {}
-        errorMessage = errorMessage || 'Unknown debugger protocol error.';
-        return reject(new Error(`Protocol error (${command}): ${errorMessage}`));
-      }
-      resolve(result);
-    });
-  });
-};
+function handler(details) {
+  if (!enabled) return;
 
-// https://drive.google.com/corp/drive/u/0/mobile/search
+  details.requestHeaders.find(header => header.name === 'User-Agent').value = userAgent;
+  enabled = false;
 
-// the good stuff.
-function turnItOn() {
-  // good god this sucks without promises.
-
-  // UA.
-  return Promise.resolve().then(_ => sendCommand('Network.setUserAgentOverride', {userAgent})).then(_ => {
-    console.log('useragent in place!', arguments);
-
-    return sendCommand('Page.navigate', {url: popUrl}).then(_ => {
-      console.log('navigated!', arguments);
-      chrome.debugger.detach({tabId});
-    });
-  });
+  return {
+    requestHeaders: details.requestHeaders
+  };
 }
 
-function launchPopup(tab) {
-  chrome.windows.create(
-    {
-      height: height,
-      width: width,
-      // "top" : 100,
-      // "left" : 100,
-      type: 'popup',
-      url: 'about:blank'
-    },
-    function(windows) {
-      tabId = windows.tabs[0].id;
-      setTimeout(attachDebugger, 20);
-    }
-  );
-}
+chrome.webRequest.onBeforeSendHeaders.addListener(handler, {
+  urls: [driveSearchURL]
+}, ["blocking", "requestHeaders"]);
 
-// this sets up the debugger. attached to all the things.
-function attachDebugger() {
-  var protocolVersion = '1.1';
-  chrome.debugger.attach({tabId}, protocolVersion, function() {
-    if (chrome.runtime.lastError) {
-      console.log(chrome.runtime.lastError.message);
-      return;
-    }
-    // 2. Debugger attached, now prepare for modifying the UA
-    turnItOn();
+
+function launchPopup() {
+  enabled = true;
+
+  chrome.windows.create({
+    height: height,
+    width: width,
+    type: 'popup',
+    url: driveSearchURL
+  }, function(windows) {
+        // window started, but navigation not yet started
   });
 }
 
@@ -85,8 +47,5 @@ chrome.runtime.onInstalled.addListener(function(details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-//chrome.browserAction.setBadgeText({text: 'mobile'});
-
+chrome.browserAction.setBadgeText({text: 'drive'});
 chrome.browserAction.onClicked.addListener(launchPopup);
-
-console.log("'Allo 'Allo! Event Page for Browser Action");
